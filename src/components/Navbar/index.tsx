7d1d4cf4501c9f4f683ae7d8e2dc33ae53f58a84
Evelyn Fernada <evelyn-cardoso@outlook.com.br>
2022-10-27 21:03:38 -0300

    refactor navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,23 @@
 import styles from "./styles.module.scss";
+import { useRouter } from 'next/router'
 import LogoSvg from "../../assets/shared/logo-svg.svg";
 import Link from "next/link";
 
 const Navbar = () => {
+  const router = useRouter()
+
   return (
     <div className={styles.navbar_container}>
     <LogoSvg />
       <ul className={styles.navbar_items}>
-        <li><Link href="/" >HOME</Link></li>
-        <li><Link href="/destination/">DESTINATION</Link></li>
-        <li><Link href="/">CREW</Link></li>
-        <li><Link href="/">TECHNOLOGY</Link></li>
+        <li aria-selected={router.pathname == '/' ? 'true' : 'false'}><Link href="/" >HOME</Link></li>
+        <li aria-selected={router.pathname == '/destination' ? 'true' : 'false'}><Link href="/destination/">DESTINATION</Link></li>
+        <li aria-selected={router.pathname == '/crew' ? 'true' : 'false'}><Link href="/crew/">CREW</Link></li>
+        <li aria-selected={router.pathname == '/technology' ? 'true' : 'false'}><Link href="/technology/">TECHNOLOGY</Link></li>
       </ul>
     </div>
   );
 };
+// {router.pathname == '/destination/' ? 'true' : 'false'}
 
 export default Navbar;
