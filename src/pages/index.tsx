17d639c6c792252d1d0c46b159e8cf617a18b113
Evelyn Fernada <evelyn-cardoso@outlook.com.br>
2022-11-15 19:44:25 -0300

    feat: home version mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,22 +13,24 @@ const Home: NextPage = () => {
       style={{ backgroundImage: `url(${bgDesktop.src})` }}
     >
       <Navbar />
-      <main className={styles.home_container}>
-        <FadeIn>
-        <div className={styles.home_content}>
-          <h3>SO, YOU WANT TO TRAVEL TO</h3>
-          <h1>SPACE</h1>
-         
-          <p>
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
-        </div>
-        </FadeIn>
-        <ButtonPulse>EXPLORE</ButtonPulse>
-      </main>
+      <FadeIn>
+        <main className={styles.home_container}>
+          <div className={styles.home_content}>
+            <h3>SO, YOU WANT TO TRAVEL TO</h3>
+            <h1>SPACE</h1>
+
+            <p>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <div className={styles.home_buttonContainer}>
+            <ButtonPulse>EXPLORE</ButtonPulse>
+          </div>
+        </main>
+      </FadeIn>
     </div>
   );
 };
