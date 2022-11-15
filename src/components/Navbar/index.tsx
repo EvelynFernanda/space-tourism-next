4d8ee239880280ab75a64383afffee92253f6fc0
Evelyn Fernada <evelyn-cardoso@outlook.com.br>
2022-11-15 19:42:46 -0300

    feat: navbar hamburger

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,22 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { FiMenu } from "react-icons/fi";
 
 import LogoSvg from "../../assets/shared/logo-svg.svg";
 
 import styles from "./styles.module.scss";
+import NavbarHamburger from "../NavbarHamburger";
 
 const Navbar = () => {
   const router = useRouter();
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const isMobile = window.innerWidth < 700;
+    setMobile(isMobile);
+  }, []);
+  console.log(mobile);
 
   const navContent = [
     {
@@ -28,18 +38,26 @@ const Navbar = () => {
   ];
 
   return (
-    <div className={styles.navbar_container}>
-      <LogoSvg />
-      <ul className={styles.navbar_items}>
-        {navContent.map((nav) => (
-          <li
-            aria-selected={router.pathname == nav.pathname ? "true" : "false"}
-          >
-            <Link href={nav.pathname}>{nav.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {mobile ? (
+        <NavbarHamburger navContent={navContent} />
+      ) : (
+        <div className={styles.navbar_container}>
+          <LogoSvg className={styles.logo} />
+          <ul className={styles.navbar_items}>
+            {navContent.map((nav) => (
+              <li
+                aria-selected={
+                  router.pathname == nav.pathname ? "true" : "false"
+                }
+              >
+                <Link href={nav.pathname}>{nav.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ src/components/NavbarHamburger/index.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { AiOutlineClose } from "react-icons/ai";
+import LogoSvg from "../../assets/shared/logo-svg.svg";
+
+import styles from "./styles.module.scss";
+import Link from "next/link";
+
+type navContentType = {
+  navContent: {
+    pathname: string;
+    name: string;
+  }[];
+};
+
+const NavbarHamburger = ({ navContent }: navContentType) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+  return (
+    <>
+      <div className={styles.navbarHamburger}>
+        <LogoSvg className={styles.logo} />
+        {!isOpen && <FiMenu className={styles.iconMenu} onClick={toggleOpen} />}
+      </div>
+      {isOpen && (
+        <div className={styles.navbarHamburger_container}>
+          <AiOutlineClose className={styles.iconMenu} onClick={toggleOpen} />
+          <ul className={styles.navbarHamburger_items}>
+            {navContent.map((nav) => (
+              <li>
+                <Link href={nav.pathname}>{nav.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default NavbarHamburger;
