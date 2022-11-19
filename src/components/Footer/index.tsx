11898d2044d0993d7b375186fa321a858b768d67
Evelyn Fernada <evelyn-cardoso@outlook.com.br>
2022-11-18 22:45:06 -0300

    fix: footer responsive

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,13 @@
+import useMobile from "../../hooks/useMobile";
 import styles from "./styles.module.scss";
 
 const Footer = () => {
+  const { isMobile } = useMobile();
+
   return (
     <footer className={styles.footer}>
       <a
+        style={isMobile ? { position: "unset" } : { position: "absolute" }}
         href="https://www.linkedin.com/in/evelyn-fernanda-cardoso/"
         target="_blank"
         rel="noopener noreferrer"
